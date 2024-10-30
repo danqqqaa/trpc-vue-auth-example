dbe4df65caa20eba308ded73860c6dcddcaff763
danqqqaa <mathilda1337@mail.ru>
2024-10-30 15:37:14 +0500

    redis init

--- apps/server-limit/src/index.ts
+++ apps/server-limit/src/index.ts
@@ -4,10 +4,14 @@ import  createContext from '../src/trpc/context'
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import cors from 'cors';
 import { appConfig } from "./config";
+import { redis } from "./services/redis";
 
 
 async function startServer() {
   try {
+
+    await redis.init();
+
     const httpServer = createHTTPServer({
       router: appRouter,
       middleware: cors(),
